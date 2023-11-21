daa4efa6a8dea4bee3cea3501aa478403940d376
Renata <renata.bancarzewska@op.pl>
2023-11-21 22:38:25 +0100

    Initial commit

---
+++ src/CreateGui.java
@@ -0,0 +1,8 @@
+import javax.swing.JFrame; // then press Enter. You can now see whitespace characters in your code.
+public class CreateGui {
+    public static void main(String[] args) {
+        JFrame frame=new JFrame();
+        frame.setSize(640, 480);
+        frame.setVisible (true);
+    }
+}
